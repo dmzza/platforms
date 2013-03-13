76025754fd86012d0b15cd1924d289b27e9bd98a
David Mazza <github@dmzza.com>
2013-03-13 05:09:59 -0400

    First stop. Stations have been created with all tests passing.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Platforms::Application.config.secret_token = '9626a4d72581f97e3b58b86651ed546548a258bee62900da3071c67ffeaa6c0916ee23a0ce9ecfab42d357f8c050167f488147f51ea3debf7d9dbeeaf8260688'
